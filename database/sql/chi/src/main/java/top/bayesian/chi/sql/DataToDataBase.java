0647d3569afffefe021bd34af777e4933a16d909
Minchao Zhu <zhuminchao@aliyun.com>
2020-03-18 00:57:51 -0500

    sql: add sql queries

---
+++ database/sql/chi/src/main/java/top/bayesian/chi/sql/DataToDataBase.java
@@ -0,0 +1,13 @@
+package top.bayesian.chi.sql;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataToDataBase 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ database/sql/chi/src/test/java/top/bayesian/chi/sql/DataToDataBaseTest.java
@@ -0,0 +1,20 @@
+package top.bayesian.chi.sql;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DataToDataBaseTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
